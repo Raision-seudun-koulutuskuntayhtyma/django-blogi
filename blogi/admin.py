926ab67bbc927031917bc5863afb59fdac443855
Tuomas Suutari <tuomas@nepnep.net>
2023-02-06 12:55:04 +0200

    Lisää admin-palikka Postaus-modelille

--- blogi/admin.py
+++ blogi/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Postaus
+
+
+@admin.register(Postaus)
+class PostausAdmin(admin.ModelAdmin):
+    list_display = ["otsikko", "luotu"]
