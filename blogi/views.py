a58d132a579dfd6574e66e501306141112d92788
Tuomas Suutari <tuomas@nepnep.net>
2023-02-06 14:58:41 +0200

    Lisää näkymät postauslistalle ja postaukselle

--- blogi/views.py
+++ blogi/views.py
@@ -1,6 +1,16 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+
+from .models import Postaus
 
 
 def postaukset(request):
-    return HttpResponse("Moi!")
+    postaukset = Postaus.objects.all()
+    context = {"postaukset": postaukset}
+    return render(
+        request,
+        "blogi/postauslista.html",
+        context,
+    )
+
+def nayta_postaus(request, id):
+    return render(request, "blogi/postaus.html")
--- koodausblogi/urls.py
+++ koodausblogi/urls.py
@@ -22,4 +22,5 @@ from blogi import views as blogi_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('postaukset/', blogi_views.postaukset),
+    path('postaus/<int:id>', blogi_views.nayta_postaus),
 ]
