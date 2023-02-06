b99487b17429584f6cccbecf87be14ed6a0b86b2
Tuomas Suutari <tuomas@nepnep.net>
2023-02-06 13:30:49 +0200

    Lisää postaukset-näkymän tynkä

--- koodausblogi/urls.py
+++ koodausblogi/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from blogi import views as blogi_views
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('postaukset/', blogi_views.postaukset),
 ]
