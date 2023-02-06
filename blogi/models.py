0e7212dc4c42f57e9047636390882c9d3010dc15
Tuomas Suutari <tuomas@nepnep.net>
2023-02-06 12:54:45 +0200

    Lisää Postaus-modeli

--- blogi/models.py
+++ blogi/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Postaus(models.Model):
+    otsikko = models.CharField(max_length=200)
+    teksti = models.TextField()
+    luotu = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.otsikko
