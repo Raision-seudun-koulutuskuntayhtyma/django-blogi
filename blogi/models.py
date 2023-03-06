ca03ec9a71f9353e65d6b3529e36a1ed4eb8ae2b
Tuomas Suutari <tuomas@nepnep.net>
2023-03-06 09:42:34 +0200

    Lisää Postaus-modeliin kuva-kenttä

--- blogi/models.py
+++ blogi/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Postaus(models.Model):
     otsikko = models.CharField(max_length=200)
     teksti = models.TextField()
+    kuva = models.ImageField(null=True)
     luotu = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
