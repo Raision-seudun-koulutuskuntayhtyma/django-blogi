f0f2e8deb5b73870c838bed65aa10b892d2776cc
Tuomas Suutari <tuomas@nepnep.net>
2023-03-06 11:05:37 +0200

    Lisää upload_to kuva-kenttään

--- blogi/models.py
+++ blogi/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Postaus(models.Model):
     otsikko = models.CharField(max_length=200)
     teksti = models.TextField()
-    kuva = models.ImageField(null=True)
+    kuva = models.ImageField(upload_to="blogi/kuvat", null=True)
     luotu = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
