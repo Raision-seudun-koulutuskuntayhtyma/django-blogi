091954c33dc8e999bd5bdf51c9b9d98e3601dd18
Tuomas Suutari <tuomas@nepnep.net>
2023-03-20 09:39:34 +0200

    Älä yritä luoda adminia uudestaan

--- create_admin_user.py
+++ create_admin_user.py
@@ -10,11 +10,21 @@ def main():
     django.setup()
 
     username = os.environ.get('ADMIN_USER_NAME', 'admin')
-    password = os.environ['ADMIN_USER_PASSWORD']
+    password = os.environ.get('ADMIN_USER_PASSWORD')
     email = os.environ.get('ADMIN_USER_EMAIL', '')
 
+    users = get_user_model().objects
+
+    if users.filter(username=username).exists():
+        print(f"User {username!r} already exists")
+        return
+
+    if not password:
+        raise SystemExit("ADMIN_USER_PASSWORD environment variable is needed!")
+
     print(f"Creating superuser {username!r}")
-    get_user_model().objects.create_superuser(username, email, password)
+    users.create_superuser(username, email, password)
+
 
 if __name__ == '__main__':
     main()
